064b26d5a87538e93169163c4ccd5d726a2c310a
senitskiy <po4ta4dima@yandex.ru>
2021-02-21 20:57:01 +0300

    compile RootTokenContract

--- DEXgastracker.sol
+++ DEXgastracker.sol
@@ -16,44 +16,43 @@ contract DEXgastracker is IDEXclient {
 		_;
 	}
 
-  address walletAddr;
+  mapping (address => address[]) rootWallets;
 	uint128 tongrams1;
 	uint128 tongrams2;
-    
-	uint address_rootA = 0xcf5d9b0d7fe0dd14f8d75b9c511fab9805ae64dc4c1f08b955c69e44193518a1;
-    address rootA = address(address_rootA);
-    uint address_rootB =  0xeaa4b8e54760d2922d6e23da188d7a2c6824ed108a7c15be5de7b97d9740253e;
-    address rootB = address(address_rootB);
 
-	function setTongrams(uint128 msgGramPrice1, uint128 msgGramPrice2) public alwaysAccept {
-		tongrams1 = msgGramPrice1;
-		tongrams2 = msgGramPrice2;
+	function setTongrams(uint128 forNewWallet, uint128 forRoot) public alwaysAccept {
+		tongrams1 = forNewWallet;
+		tongrams2 = forRoot;
 	}
 
-	function getTongrams() public view alwaysAccept returns (uint128 msgGramPrice1, uint128 msgGramPrice2){
-		msgGramPrice1 = tongrams1;
-		msgGramPrice2 = tongrams2;
+	function getTongrams() public view alwaysAccept returns (uint128 forNewWallet, uint128 forRoot){
+		forNewWallet = tongrams1;
+		forRoot = tongrams2;
 	}
 
-	function getWalletAddr() public view alwaysAccept returns (address walletValue){
-		walletValue = walletAddr;
+	function getWalletAddr(address root) public view alwaysAccept returns (address[] walletsArr){
+		walletsArr = rootWallets[root];
 	}
 
+	function createWalletId() private pure returns (uint256) {
+		rnd.shuffle();
+		return rnd.getSeed();
+	}
 
-	function createNewEmptyWallet(bool selector) public view alwaysAccept  returns (bool createStatus) {
+	function createNewEmptyWallet(address root) public view alwaysAccept  returns (bool createStatus) {
 		createStatus = false;
-		address creator = (selector == true)?rootA:rootB;
+		address creator = root;
+		uint256 walletId = createWalletId();
 		address owner = address(this);
 		uint256 ownerUINT = owner.value;
-		TvmCell body = tvm.encodeBody(IRootTokenContract(creator).deployEmptyWallet, 0x00000007, 0, 0, ownerUINT, tongrams1);
+		TvmCell body = tvm.encodeBody(IRootTokenContract(creator).deployEmptyWallet, 0x00000007, 0, walletId, ownerUINT, tongrams1);
 		creator.transfer({value:tongrams2, bounce:false, body:body});
 		createStatus = true;
 	}
 
 	function setNewEmptyWallet(address value0) public override alwaysAccept functionID(0x00000007){
 		address root = msg.sender;
-		walletAddr = value0;
+		rootWallets[root].push(value0);
 	}
 
 }
-// compl + depl 100TON
